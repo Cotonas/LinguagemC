29a80e2734d177d14c3cc77cdabe3ddcaaefcc76
Guilherme Bandeira Ludovico <gui-bandeira@hotmail.com>
2021-04-07 21:41:11 -0300

    Conversor de moeda usando membros staticos

---
+++ ConversorMoeda/ConversorMoeda/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorMoeda
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 6.0;
+
+        public static double ConverterParaReal(double cotacao, double qtdDolar)
+        {
+            double total = cotacao * qtdDolar;
+            return total + total * Iof / 100.0;
+        }
+    }
+}
---
+++ ConversorMoeda/ConversorMoeda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a cotação do dólar atual: ");
+            double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Digite quantos doláres deseja comprar: ");
+            double qtdDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double convert = ConversorDeMoeda.ConverterParaReal(cotacao, qtdDolar);
+
+            Console.WriteLine("Valor a ser pago em reais: " + convert.ToString("F2",CultureInfo.InvariantCulture));
+
+        }
+    }
+}
