3e4e3fc8e216ec196ba976b9c98b0230e19f8728
Guilherme Bandeira Ludovico <gui-bandeira@hotmail.com>
2021-04-27 16:02:28 -0300

    Enumeração e Composição Holerite com outros possíveis contratos por mês

---
+++ HoleriteDesign/HoleriteDesign/Entities/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace HoleriteDesign.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ HoleriteDesign/HoleriteDesign/Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace HoleriteDesign.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
