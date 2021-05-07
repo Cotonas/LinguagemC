f4438498fc047d0248b5c53d5b0a22617cd90dc3
Guilherme Bandeira Ludovico <gui-bandeira@hotmail.com>
2021-05-07 13:33:21 -0300

    Conta bancaria para exemplo de Upcasting e Downcasting

---
+++ ContaBancariaEmpresarial/ContaBancariaEmpresarial/Entities/BusinessAccount.cs
@@ -0,0 +1,25 @@
+﻿namespace ContaBancariaEmpresarial.Entities
+{
+    // Herança, subclasse de Account.
+    class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount()
+        {
+        }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if(amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+    }
+}
